207ccee74e4c409ba731707687daff1d491ba920
Ivan Szebenszki <szib77@gmail.com>
2019-06-10 10:25:48 +0100

    Search

--- src/Components/TVShowList.js
+++ src/Components/TVShowList.js
@@ -6,17 +6,9 @@ import TVShow from './TVShow'
 class TVShowList extends Component {
 
   mapAllShows = () => {
-    if (!!this.props.searchTerm){
-      // this.props.shows.map((s) => {
-      //   if (s.name.toLowerCase().includes(this.props.searchTerm)){
-      //     (<TVShow show={s} key={s.id} selectShow={this.props.selectShow}/> )
-      //   }
-      // })
-      this.props.shows
-        .filter((s) => s.name.toLowerCase().includes(this.props.searchTerm))
-        .map(s => <TVShow show={s} key={s.id} selectShow={this.props.selectShow}/> )
-    }
-    return this.props.shows.map( (s)=> <TVShow show={s} key={s.id} selectShow={this.props.selectShow}/>)
+    return this.props.shows
+      .filter((s) => s.name.toLowerCase().includes(this.props.searchTerm))
+      .map(s => <TVShow show={s} key={s.id} selectShow={this.props.selectShow}/> )
   }
 
   render() {
